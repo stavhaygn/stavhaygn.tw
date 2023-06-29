111809209cbc33432007e71fa16fe551560a9569
stavhaygn <stavhaygnzhan@gmail.com>
2023-06-29 20:15:34 +0800

    Update 2023-06-29-20:15:34

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -64,7 +64,7 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: '</>',
+        title: '\'_>\' )',
         items: [
           {
             type: 'docSidebar',
